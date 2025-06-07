d9cc1208a3bab00ce337a4981a3da50921b66cd6
Priyasoipulickal <ms22d001@smail.iitm.ac.in>
2025-06-07 15:51:25 +1000

    Update indianWhiteBIAT.js

--- indianWhiteBIAT.js
+++ indianWhiteBIAT.js
@@ -1,103 +1,81 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat5.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension){
   var API = new APIConstructor();
 
   return iatExtension({
-    practiceCategory1 : 
-      {
-        name : 'Mammals', //Will appear in the data.
-        title : {
-          media : {word : 'Mammals'}, //Name of the category presented in the task.
-          css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
-          height : 4, //Height (because we need to know where to put the next item in the title)
-          startStimulus : { 
-            //If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-            media : {word : 'Dogs, Horses, Cows, Lions'}, 
-            css : {color:'#31b404','font-size':'1em'}, 
-            height : 2
-          }
-        }, 
-        stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word : 'Dogs'}, 
-          {word : 'Horses'}, 
-          {word : 'Lions'}, 
-          {word : 'Cows'}
-        ], 
-        //Stimulus css (style of the stimuli)
-        stimulusCss : {color:'#31b404','font-size':'2em'}
-      },  
-    practiceCategory2 : 
-      {
-        name : 'Birds', 
-        title : {
-          media : {word : 'Birds'}, 
-          css : {color:'#31b404','font-size':'1.8em'}, 
-          height : 4,
-          startStimulus : {
-            media : {word : 'Pigeons, Swans, Crows, Ravens'}, 
-            css : {color:'#31b404','font-size':'1em'}, 
-            height : 2
-          }
-        }, 
-        stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word : 'Pigeons'}, 
-          {word : 'Swans'}, 
-          {word : 'Crows'}, 
-          {word : 'Ravens'}
-        ], 
-        //Stimulus css
-        stimulusCss : {color:'#31b404','font-size':'2em'}
+    category1 : {
+      name : 'Indians',
+      title : {
+        media : {word : 'Indians'},
+        css : {color:'#31b404','font-size':'1.8em'},
+        height : 4
       },
-    categories : [  //As many categories you need.
-      {
-        name : 'Indians', //Will appear in the data.
-        title : {
-          media : {word : 'Indians'}, //Name of the category presented in the task.
-          css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
-          height : 4, //Height (because we need to know where to put the next item in the title)
-          startStimulus : { 
-            //Using word list instead of image for clarity
-            media : {word : 'Ravi, Priya, Anil, Lakshmi'}, 
-            css : {color:'#31b404','font-size':'1em'}, 
-            height : 2
-          }
-        }, 
-        stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word : 'Ravi'}, 
-          {word : 'Priya'}, 
-          {word : 'Anil'}, 
-          {word : 'Lakshmi'},
-          {word : 'Amit'},
-          {word : 'Sanjay'}
-        ], 
-        //Stimulus css (style of the stimuli)
-        stimulusCss : {color:'#31b404','font-size':'2em'}
-      },  
-      {
-        name : 'Whites', 
-        title : {
-          media : {word : 'Whites'}, 
-          css : {color:'#31b404','font-size':'1.8em'}, 
-          height : 4,
-          startStimulus : {
-            media : {word : 'Charles, Emily, Mike, Nancy'},  
-            css : {color:'#31b404','font-size':'1em'}, 
-            height : 2
-          }
-        }, 
-        stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word : 'Charles'}, 
-          {word : 'Emily'}, 
-          {word : 'Mike'}, 
-          {word : 'Nancy'},
-          {word : 'Laura'},
-          {word : 'Richard'}
-        ], 
-        //Stimulus css
-        stimulusCss : {color:'#31b404','font-size':'2em'}
-      }
-    ],
-    base_url : {//Where are your images at?
+      stimulusMedia : [
+        {word : 'Rajesh'},
+        {word : 'Priya'},
+        {word : 'Amit'},
+        {word : 'Deepa'},
+        {word : 'Ravi'},
+        {word : 'Sunita'}
+      ],
+      stimulusCss : {color:'#31b404','font-size':'2em'}
+    },
+
+    category2 : {
+      name : 'Whites',
+      title : {
+        media : {word : 'Whites'},
+        css : {color:'#31b404','font-size':'1.8em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {word : 'John'},
+        {word : 'Emma'},
+        {word : 'James'},
+        {word : 'Olivia'},
+        {word : 'William'},
+        {word : 'Grace'}
+      ],
+      stimulusCss : {color:'#31b404','font-size':'2em'}
+    },
+
+    attribute1 : {
+      name : 'Good',
+      title : {
+        media : {word : 'Good'},
+        css : {color:'#0000FF','font-size':'1.8em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {word : 'Joy'},
+        {word : 'Love'},
+        {word : 'Peace'},
+        {word : 'Wonderful'},
+        {word : 'Pleasure'},
+        {word : 'Glorious'}
+      ],
+      stimulusCss : {color:'#0000FF','font-size':'2em'}
+    },
+
+    attribute2 : {
+      name : 'Bad',
+      title : {
+        media : {word : 'Bad'},
+        css : {color:'#FF0000','font-size':'1.8em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {word : 'Horrible'},
+        {word : 'Nasty'},
+        {word : 'Anger'},
+        {word : 'Evil'},
+        {word : 'Failure'},
+        {word : 'Terrible'}
+      ],
+      stimulusCss : {color:'#FF0000','font-size':'2em'}
+    },
+
+    base_url : {
       image : 'https://baranan.github.io/minno-tasks/images/'
-    } 
+    }
   });
 });
